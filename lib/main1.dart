0ebbb41a2d1b91e8e84e845ce1589997d5a3c756
Krinesh <pkrinesh95@gmail.com>
2022-10-29 16:35:37 +0530

    feat: rewrite the counter code

---
+++ lib/main1.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+        title: "Flutter Layout Constrains",
+        theme:ThemeData(
+            primarySwatch: Colors.indigo
+        ),
+        home: const HomePage(homePageTitle: "Counter",)
+    );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key, required this.homePageTitle});
+
+  final String homePageTitle;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.homePageTitle),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('You have pushed the button this many times:'),
+              Text('$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              )
+            ],
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState((){
+            _counter++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
